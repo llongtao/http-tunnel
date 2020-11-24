c155c377cc12daf9e796b0b5861ae6e6c31e08d4
nicolas-dutertry <nicolas@dutertry.com>
2020-11-24 18:03:19 +0100

    Refactoring

--- htunnel-server/src/main/java/com/dutertry/htunnel/server/controller/TunnelController.java
+++ htunnel-server/src/main/java/com/dutertry/htunnel/server/controller/TunnelController.java
@@ -56,7 +56,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
